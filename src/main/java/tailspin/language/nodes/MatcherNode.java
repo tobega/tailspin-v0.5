994c0605b5e113d8f1303c823243e3d76952067d
tobega <torbjorn.gannholm@gmail.com>
2024-06-29 13:09:56 +0200

    big number comparisons

--- src/main/java/tailspin/language/nodes/MatcherNode.java
+++ src/main/java/tailspin/language/nodes/MatcherNode.java
@@ -1,7 +1,9 @@
 package tailspin.language.nodes;
 
+import com.oracle.truffle.api.dsl.TypeSystemReference;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
+@TypeSystemReference(TailspinTypes.class)
 public abstract class MatcherNode extends TailspinNode {
   public abstract boolean executeMatcherLong(VirtualFrame frame, long toMatch);
   public abstract boolean executeMatcherGeneric(VirtualFrame frame, Object toMatch);
