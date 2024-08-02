f2675a2f0792ead0e45c746734418a96af4d9c2d
tobega <torbjorn.gannholm@gmail.com>
2024-08-02 09:15:57 +0200

    Use dynamic object library for write

--- src/main/java/tailspin/language/nodes/structure/WriteStructurePropertyNode.java
+++ src/main/java/tailspin/language/nodes/structure/WriteStructurePropertyNode.java
@@ -1,28 +1,28 @@
 package tailspin.language.nodes.structure;
 
+import com.oracle.truffle.api.dsl.Cached.Shared;
 import com.oracle.truffle.api.dsl.GenerateInline;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.UnknownIdentifierException;
-import com.oracle.truffle.api.interop.UnsupportedMessageException;
-import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.Node;
-import tailspin.language.TypeError;
+import com.oracle.truffle.api.object.DynamicObjectLibrary;
 import tailspin.language.runtime.Structure;
 
 @GenerateInline
 public abstract class WriteStructurePropertyNode extends Node {
   public abstract Structure executeWriteProperty(Node node, Structure target, String key, Object value);
 
-  @Specialization(limit = "2")
+  @Specialization
+  protected Structure writeLong(Structure target, String key, long value,
+      @CachedLibrary(limit = "2") @Shared DynamicObjectLibrary dynamicObjectLibrary) {
+    dynamicObjectLibrary.putLong(target, key, value);
+    return target;
+  }
+
+  @Specialization
   protected Structure writeProperty(Structure target, String key, Object value,
-      @CachedLibrary("target") InteropLibrary interopLibrary) {
-    try {
-      interopLibrary.writeMember(target, key, value);
-    } catch (UnsupportedMessageException | UnsupportedTypeException | UnknownIdentifierException e) {
-      throw new TypeError(e.getMessage());
-    }
+      @CachedLibrary(limit = "2") @Shared DynamicObjectLibrary dynamicObjectLibrary) {
+    dynamicObjectLibrary.put(target, key, value);
     return target;
   }
 }
